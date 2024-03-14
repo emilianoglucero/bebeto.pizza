61070034ff4a70ede9f185818813528352c9a2a8
emiliano lucero <emilianoglucero@gmail.com>
2024-03-14 16:36:36 -0300

    Remove unused plugin and update build options

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { defineConfig } from "vite";
 import usePHP from "vite-plugin-php";
 
 export default defineConfig({
-  plugins: [usePHP()],
   root: "./",
   publicDir: "../static/",
   base: "./",
@@ -15,15 +14,18 @@ export default defineConfig({
     open: !isCodeSandbox, // Open if it's not a CodeSandbox
   },
   build: {
-    outDir: "../dist",
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        // test: resolve(__dirname, "pages", "test", "index.html"),
-        // test: resolve(__dirname, "pages", "test/index.html"),
-        test: resolve(__dirname, "pages/test/index.html"),
+        compraventa: resolve(__dirname, "pages", "compraventa", "index.html"),
+        maradona: resolve(__dirname, "pages", "maradona", "index.html"),
+        zine1: resolve(__dirname, "pages", "zine1", "index.html"),
+        voca: resolve(__dirname, "pages", "voca", "index.html"),
+        screenshots: resolve(__dirname, "pages", "screenshots", "index.html"),
+        zarandraca: resolve(__dirname, "pages", "zarandraca", "index.html"),
+        dibuja: resolve(__dirname, "pages", "dibuja", "index.html"),
       },
     },
   },
